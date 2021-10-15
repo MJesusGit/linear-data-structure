2049b933a18d6ecc27ca6a4c45d71ff9ab59d33d
AndresGonzalez23 <72602354+AndresGonzalez23@users.noreply.github.com>
2021-10-15 02:30:24 +0200

    Primer ReadFile Array

--- ReadFile.java
+++ ReadFile.java
@@ -3,31 +3,32 @@ package stacks;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.*;
 
 public class ReadFile {
-    public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("C:\\naturales.dat.txt");
-        Scanner reader = new Scanner(file);
-        Stack<Integer> file_stack = new Stack();
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("C:\\naturales.dat.txt");
+		Scanner reader = new Scanner(file);
+		int counter = 0;
 
-        try {
-            // we will read the file
-            System.out.println("...Reading the file...");
+		try {
+			// we will read the file
+			System.out.println("...Reading the file...");
 
-            // we read number a number
-            while (reader.hasNext()) {
-                int number = reader.nextInt();
-                file_stack.push(number);
+			// we read number a number
+			while (reader.hasNext()) {
+				int number = reader.nextInt();
+				counter += 1;
+				System.out.print(" " + number);
 
-            }
-
-            while(!file_stack.empty()) {
-                System.out.print(" "+file_stack.pop());
-            }
-
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
+				int[] auxiliar_Number = new int[counter];
+				for(int i=0; i<counter; i++) {
+					auxiliar_Number[i]=number;
+					
+				}
+			}
+			System.out.println("\n"+counter);
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
 }
