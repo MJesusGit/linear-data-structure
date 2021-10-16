2b376226e3ad8b25596f414a319d8cfa436aee32
AndresGonzalez23 <72602354+AndresGonzalez23@users.noreply.github.com>
2021-10-16 04:07:43 +0200

    File Reader optimal version and method for stacks
    Creation of the method to fill the first and the second stacks in the class Problem
    The method to read the files has been updated
    The main class is still empty

---
+++ Problem.java
@@ -0,0 +1,43 @@
+package stacks;
+
+import java.util.*;
+import java.io.*;
+
+public class Problem {	
+	
+	/*************************************************************************************************************
+	 * * * Method name: fill_Stacks * * Author/s name: Andrés González Varela, María Jesús Dueñas Recuero, 		 *
+	 * Juan Gigante Ríos.																		 *
+	 * 																											 *
+	 * * * Description of the Method: This method will fill the stacks one by one checking if the addition of 	 *
+	 * the number in the array and the number on the top of the first stack is 9 or not, depending on the result *
+	 * we will increase the first or the second stack. In case the first stack is empty the number of the array  *
+	 * will be pushed on this stack. On the end we will return the second_Stack that we will have to use later   *																		 *
+	 *************************************************************************************************************/
+	
+	public Stack fill_Stacks(ReadFile file) throws FileNotFoundException {
+		int [] array = file.read_File();
+		Stack<Integer>first_Stack = new Stack<Integer>();
+		Stack<Integer>second_Stack = new Stack<Integer>();
+		
+		for (int i = 0; i < array.length; i++) {
+			if(first_Stack.isEmpty()) {
+				first_Stack.push(array[i]);
+			}else{
+				if(first_Stack.peek() + array[i] == 9){
+					for (int j = 0; j < 2; i++) {
+						if (first_Stack.peek() <= array[i]) {
+							second_Stack.push(first_Stack.pop());
+						}else {
+							second_Stack.push(array[i]);
+							first_Stack.pop();
+						}
+					}
+				}else if(first_Stack.peek() + array[i] != 9) {
+					first_Stack.push(array[i]);
+				}
+			}
+		}
+		return second_Stack;
+	}
+}
--- ReadFile.java
+++ ReadFile.java
@@ -5,30 +5,49 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class ReadFile {
-	public static void main(String[] args) throws FileNotFoundException {
-		File file = new File("C:\\naturales.dat.txt");
+
+	private String fileName = "C\\:naturales.dat.txt";
+
+	/*************************************************************************************************************
+	 * * * Method name: read_File * * Author/s name: Andrés González Varela, María Jesús Dueñas Recuero, 		 *
+	 * Juan Gigante Ríos.																					 *
+	 * 																											 *
+	 * * * Description of the Method: This method will read the file that is located in the root that it is 	 *
+	 * given. First of all the method will count how many numbers there is on the file and then it creates an 	 *
+	 * array of that lenght. After this the method reads number by number, introducing the number in the file 	 *
+	 * in the array. If the file is not found it will be indicated by terminal. 								 *
+	 *************************************************************************************************************/
+	
+	public int[] read_File() throws FileNotFoundException {
+		File file = new File(fileName);
 		Scanner reader = new Scanner(file);
 		int counter = 0;
-
+		int[] auxiliar_Number = null;
 		try {
-			// we will read the file
 			System.out.println("...Reading the file...");
-
-			// we read number a number
 			while (reader.hasNext()) {
 				int number = reader.nextInt();
 				counter += 1;
 				System.out.print(" " + number);
 
-				int[] auxiliar_Number = new int[counter];
-				for(int i=0; i<counter; i++) {
-					auxiliar_Number[i]=number;
-					
+				auxiliar_Number = new int[counter];
+				for (int i = 0; i < counter; i++) {
+					auxiliar_Number[i] = number;
+
 				}
 			}
-			System.out.println("\n"+counter);
+			System.out.println("\n" + counter);
 		} catch (Exception ex) {
 			System.out.println(ex.getMessage());
 		}
+		return auxiliar_Number;
+	}
+
+	/***************************************************************************************************************
+	 * This method is the constructor of this class																   *
+	 ***************************************************************************************************************/
+
+	public ReadFile(String fileName) {
+		this.fileName = fileName;
 	}
 }
