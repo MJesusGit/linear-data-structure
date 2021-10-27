5273e9f56102f2bea78a430385d0151480cdc9f2
AndresGonzalez23 <72602354+AndresGonzalez23@users.noreply.github.com>
2021-10-27 14:14:15 +0200

    Add files via upload

--- ReadFile.java
+++ ReadFile.java
@@ -19,36 +19,30 @@ public class ReadFile {
 	     *************************************************************************************************************/
 
 	public Stack read_File(String fileRoute) throws FileNotFoundException {
-        File file = new File(fileRoute);
-        Scanner reader_File = null;
-        Stack<Integer> aux_number_stack_file = new Stack<Integer>();
-        Stack<Integer> number_Stack_File = new Stack<Integer>();
+		File file = new File(fileRoute);
+		Scanner reader_File = null;
+		Stack<Integer> aux_number_stack_file = new Stack<Integer>();
+		Stack<Integer> number_Stack_File = new Stack<Integer>();
 
-        try {
-            reader_File = new Scanner(file);
-            while (reader_File.hasNext()) {
-                int number_File = reader_File.nextInt(); 
-                aux_number_stack_file.push(number_File);
-            }
+		try {
+			reader_File = new Scanner(file);
+			while (reader_File.hasNext()) {
+				int number_File = reader_File.nextInt();
+				aux_number_stack_file.push(number_File);
+			}
 
-            for (int i = 0; 0 < aux_number_stack_file.size(); i++) {
+			for (int i = 0; 0 < aux_number_stack_file.size(); i++) {
 
-                number_Stack_File.push(aux_number_stack_file.pop());
+				number_Stack_File.push(aux_number_stack_file.pop());
 
-            }
+			}
 
-            System.out.println(number_Stack_File.peek());
-            for (int j = 0; 0 < number_Stack_File.size(); j++) {
+		} catch (Exception ex) {
+			System.out.println("Mensaje: " + ex.getMessage());
 
-                System.out.print(number_Stack_File.pop());
-            }
-
-        } catch (Exception ex) {
-            System.out.println("Mensaje: " + ex.getMessage());
-           
-        }
-        return number_Stack_File;
-    }
+		}
+		return number_Stack_File;
+	}
 
 	public ReadFile(String fileName) {
 		this.fileName = fileName;
