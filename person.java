1b18b913292d0352a3d4e917ac8c11e9e33615b7
Juan Gigante Ríos <91609579+jgiganterios@users.noreply.github.com>
2021-11-03 11:10:16 +0100

    Add files via upload

---
+++ person.java
@@ -0,0 +1,21 @@
+package queues;
+
+public class person {
+
+	String stagename;
+	String ticket;
+	
+	public person(String stagename, String ticket) {
+		this.stagename = stagename;
+		this.ticket = ticket;
+	}
+	
+	public void setstagename(String stagename) {
+		this.stagename = stagename;
+	}
+	
+	public void setticket(String ticket) {
+		this.ticket = ticket;
+	}
+	
+}
