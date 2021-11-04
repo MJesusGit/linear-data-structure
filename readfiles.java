053e9ead109d9e579a3416fa34608f819c9afa1b
Juan Gigante Ríos <91609579+jgiganterios@users.noreply.github.com>
2021-11-04 13:22:36 +0100

    Add files via upload

--- readfiles.java
+++ readfiles.java
@@ -7,34 +7,29 @@ import java.util.Scanner;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class readfiles {
-	
+
 	private String filename = "C\\:people.txt";
-	
-	public String[] readfile(String file) throws FileNotFoundException {
+
+	public Queue<String> readfile(String file) throws FileNotFoundException {
 
 		File f = new File(filename);
 		Scanner reader = new Scanner(f);
-		String[] ifile = null;
+
 		Queue<String> filequeue = new LinkedBlockingQueue<String>();
-		Queue<String> auxfilequeue = new LinkedBlockingQueue<String>();
 
 		try {
 
 			System.out.println("Reading the file...");
 			while (reader.hasNext()) {
 				String data = reader.next();
-				auxfilequeue.offer(data);
+				filequeue.offer(data);
 			}
-		//	for(int i = 0; i < auxfilequeue.size()) {
-		//		filequeue.offer(auxfilequeue.rear())
-		//		ifile(i) = auxfilequeue.
-		//	}
 		} catch (Exception ex) {
 			System.out.println(ex.getMessage());
 		}
 
 		reader.close();
 
-		return ifile;
+		return filequeue;
 	}
 }
