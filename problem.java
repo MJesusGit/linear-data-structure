3a4f9c9388ff7d0c50126cde42c1f716359d29f0
Juan Gigante Ríos <91609579+jgiganterios@users.noreply.github.com>
2021-11-04 12:43:22 +0100

    Add files via upload

---
+++ problem.java
@@ -0,0 +1,36 @@
+package queues;
+
+import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class problem {
+	
+	 Queue<String> queue = new LinkedBlockingQueue<String>();
+	 
+	 //crear cola de objetos personas
+	 public Queue<String> peoplequeue(String[] ifile){
+		 
+	 Queue<person> personqueue = new LinkedBlockingQueue<person>();
+	 		
+	 	for(int i=0; i < ifile.length; i++) {
+	 		
+	 	}
+	 
+	 return peoplequeue;
+	 }
+	 //distribucion de personas en diferentes filas
+	 //
+	 Queue<person> coliseumE1 = new LinkedBlockingQueue<person>();
+	 Queue<person> roarofthesea = new LinkedBlockingQueue<person>();
+	 Queue<person> sirensong = new LinkedBlockingQueue<person>();
+	 
+	 for (int i = 0; i < personqueue.size(); i++) {
+		 
+	 }
+	 //cálculo del tiempo de cada fila
+	 
+	public problem(Queue<String> queue) {
+		this.queue = queue;
+	}
+
+}
