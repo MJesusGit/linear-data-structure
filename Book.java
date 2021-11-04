f41c0504871236317a0043c5e95b84150e5c8527
mjpllano <72667996+MJesusGit@users.noreply.github.com>
2021-11-04 13:41:13 +0100

    ReadFile & book classes incompleted

---
+++ Book.java
@@ -0,0 +1,36 @@
+package list;
+
+public class Book {
+	private String author;
+	private String tittle;
+	private double rating;
+	
+		
+	public Book(String author, String tittle, double rating) {
+		super();
+		this.author = author;
+		this.tittle = tittle;
+		this.rating = rating;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getTittle() {
+		return tittle;
+	}
+	public void setTittle(String tittle) {
+		this.tittle = tittle;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	
+	
+	
+}
